0411d27777c38e7a145180d4954e45ea14838f06
Richard Orton <richard.orton@glasgow.ac.uk>
2018-08-24 16:40:59 +0100

    Update align.sh

--- align.sh
+++ align.sh
@@ -1,8 +1,8 @@
 
 bowtie2-build $1 $1
 
-bowtie2 -p 5 --local -x $1 -1 CSF_S1_R1_val_1.fq -2 CSF_S1_R2_val_2.fq -S ${1}.sam
-#bowtie2 -p 5 --local -x $1 -1 $2 -2 $3 -S ${1}.sam
+#bowtie2 -p 5 --local -x $1 -1 CSF_S1_R1_val_1.fq -2 CSF_S1_R2_val_2.fq -S ${1}.sam
+bowtie2 -p 5 --local -x $1 -1 $2 -2 $3 -S ${1}.sam
 
 samtools view -@ 5 -bS ${1}.sam > ${1}.bam
 rm -f ${1}.sam
